dd87e7e9c0c88e38a68c49148fa5636a5056e190
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-09 18:32:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.text('Omega 3 & Blueberry oatmeal')
 st.text('Kale,Spinach & rocket Smoothie')
 st.text('hard-Boiled Free-range Egg')
 
-st.header('\N{banana}\N{mango} Breakfast of Champion Coders \N{grapes}')
+st.header('\N{banana}\N{mango} Breakfast of Champion Coders \N{grapes}\N{grapes}')
