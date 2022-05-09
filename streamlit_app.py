1d1b30ac4acf9b2d2878a6a87b655e47644ca48e
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-09 15:30:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 st.title('Breakfast Menu')
 st.title('Omega 3 & Blueberry oatmeal')
+st.title('Kale,Spinach & rocket Smoothie')
+st.title('hard-Boiled Free-range Egg')
