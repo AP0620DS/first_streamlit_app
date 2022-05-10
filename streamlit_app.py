6336a8018c84613592b45a541a576b667697aeb0
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-09 21:11:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ st.text('Omega 3 & Blueberry oatmeal')
 st.text('Kale,Spinach & rocket Smoothie')
 st.text('hard-Boiled Free-range Egg')
 
-st.header('\U0001F34C \U0001F96D \U0001F352 Build Your Own Fruit Smoothie \U0001F95D \U0001F34A')
+st.header('\U0001F34C \U0001F96D Build Your Own Fruit Smoothie \U0001F95D \U0001F34A')
