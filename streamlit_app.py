2dcdc8872fc66ce6948fe925ff30c62ff82572ac
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-09 21:15:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit 
+import pandas
 streamlit.header('\U0001F600 Breakfast Favorites')
 streamlit.text('\U0001F423 Omega 3 & Blueberry Oatmeal')
 streamlit.text('\U0001F33F Kale , spinach & Rocket Smoothie')
 streamlit.text('\U0001F95A Hard-Boiled Free Range Egg')
 streamlit.text('\U0001F951 Avacado Toast')
 streamlit.header('\U0001F34C \U0001F96D Build Your Own Fruit Smoothie \U0001F95D \U0001F34A')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
