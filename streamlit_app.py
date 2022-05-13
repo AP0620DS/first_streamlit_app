dfb2419d396ecb385e5d48ee6ccc524608181667
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-13 10:51:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,15 @@ streamlit.text(fruityvice_response)
 
 
 
+#New Section to display frityvice api response 
+
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit whould you like information about ?' ,'Kiwi')
 streamlit.write('The user entered',fruit_choice)
+                                                     
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+                                    
                                                      
 
  
