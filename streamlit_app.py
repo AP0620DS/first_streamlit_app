02fab953ec56cfd56d2ae34248baf8fb2afcffab
AP0620DS <105241402+AP0620DS@users.noreply.github.com>
2022-05-13 10:26:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response 
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelom")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
 
@@ -28,5 +28,5 @@ streamlit.header('Fruityvice Fruit Advice !')
 
 
 import requests 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelom")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
